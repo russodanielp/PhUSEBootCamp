254c305326222ebd78bf640f5242f3fe83f679ef
Daniel P. Russo <russodanielp@gmail.com>
2025-03-27 01:03:25 +0000

    started day 1

---
+++ Day1.R
@@ -0,0 +1,20 @@
+# The basics 
+# R as a calculator
+
+# running code
+2+2
+
+# assigning variables 
+result <- 2+2
+result
+
+# types
+aString <- "This is a string"
+aString
+
+# functions
+
+ls()
+
+# getting help 
+?"<-"
