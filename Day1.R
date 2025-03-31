36c43212b76a7d9989c172703dcf11da1dc9873a
Daniel P. Russo <russodanielp@gmail.com>
2025-03-31 00:11:50 +0000

    refactored some of day 1 into its own script

--- Day1.R
+++ Day1.R
@@ -1,5 +1,5 @@
 # The basics 
-# R as a calculator
+# R can be a calculator
 
 # running code
 2+2
@@ -14,7 +14,49 @@ aString
 
 # functions
 
-ls()
+myVector <- c(1, 2)
+
+myNumbers <- 1:10
+
+# operators
+
+print(myNumbers)
+
+
 
 # getting help 
 ?"<-"
+
+# installing packages
+
+# install.packages('haven')
+# install.packages('ggplot2')
+# install.packages('dplyr')
+# 
+
+# loading packakges
+library(haven)
+
+
+# reading an XPT file.  Lets
+# just take a look at BW 
+haven::read_xpt('data/bg.xpt')
+
+bw <- haven::read_xpt('data/bw.xpt')
+
+head(bw)
+View(bw)
+
+
+# think of columns as vectors 
+bw$USUBJID
+
+bw['USUBJID']
+
+# we can do operations on vectors
+bw['BWSTRESN'] / 1000
+
+# we can create new columns
+# via assignment
+bw['BWSTRESN_KG'] <- bw['BWSTRESN'] / 1000
+
