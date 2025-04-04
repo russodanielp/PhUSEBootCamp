8b34e8fdfcfa0dcc12dc1d3a5cd5bf8e8f1f37d7
Daniel P. Russo <russodanielp@gmail.com>
2025-04-04 11:07:14 +0000

    finalized day 1

--- Day1.R
+++ Day1.R
@@ -1,27 +1,65 @@
+# Welcome to Day 1 of the PhUSE SEND
+# Coding Bootcamp!  This is your first 
+# R Script!  Below are different R commands,
+# and anything preceded by a # is a 'comment'
+# and effectively ignored by the R interpreter.
+# These are good for documenting code.
+# Good luck and happy coding! 
+
 # The basics 
 # R can be a calculator
 
-# running code
+# running code - this
+# will get displayed to the 
+# console
 2+2
 
-# assigning variables 
+# assigning variables allows 
+# information to be 'stored' in 
+# a 'container'.  This is generally 
+# done by an assignment operation using <-.
+# they can be explicit or results of operations
+myNumber <- 2
+myNumber
 result <- 2+2
 result
+
+# they can be very powerful and allow
+# for more complex code
+resultNumTwo <- myNumber + 2
+resultNumTwo
+
+# but they can always be reassigned
+myNumber <- 100
+myNumber
 
 # types
 aString <- "This is a string"
 aString
 
-# functions
+# operations depend on type
+# for example, this results in
+# an error:
+aString * 2
 
+
+# functions are code 
+# that take 'input' and 
+# produce an output
+# combine is a popular function
 myVector <- c(1, 2)
+myVector
 
-# vectors 
-
+# vectors can be created as a 
+# range too
 myNumbers <- 1:10
+myNumbers
 
-# getting help 
-?"<-"
+# operations on vectors
+# are different and the result
+# is element-wise
+numbersTimesTwo <- myNumbers  * 2
+numbersTimesTwo
 
 # installing packages
 # these should already be installed
